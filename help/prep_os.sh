bfdd6a96fd85b710b2a54d179f5cdcddb7b2318b
Roberto Soto <webmaster@numerica.cl>
2018-06-13 08:54:24 -0700

    fix file storage permissions these need to be set at build time, else it will belong to root

--- help/prep_os.sh
+++ help/prep_os.sh
@@ -8,20 +8,13 @@ echo "host    all             all             0.0.0.0/0 trust" >> /etc/postgresq
 echo "listen_addresses='*'" >> /etc/postgresql/9.3/main/postgresql.conf
 
 #
-# get latest Libretime
-#
-#echo "Clone libretime"
-#mkdir /libretime_src
-#cd /libretime_src
-#git clone https://github.com/LibreTime/libretime.git
-#
-#
 # activate RabbitMQ management
 #
 echo "NODENAME=rabbitmq@localhost" > /etc/rabbitmq/rabbitmq-env.conf
 rabbitmq-plugins enable rabbitmq_management
 # start rabbitmq for airtime install
 /etc/init.d/rabbitmq-server start
+
 #
 # install lastest libretime
 #
@@ -49,3 +42,7 @@ echo "Create some configfiles .."
 # define postgres password file
 echo "localhost:5432:airtime:airtime:airtime" > /root/.pgpass
 chmod 600 /root/.pgpass 
+
+# arreglar permisos
+mkdir /srv/airtime/stor
+chown -R www-data /srv/airtime/stor
